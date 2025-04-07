716f03b25dd9e07ad44adc0ef0efef6b5b2e2b48
g00dtim3 <hello@fairti.me>
2025-04-07 21:35:12 +0200

    Update app.py

--- pf_api_explorer/app.py
+++ pf_api_explorer/app.py
@@ -5,7 +5,7 @@ import datetime
 import io
 import altair as alt
 
-st.set_page_config(page_title="Explorateur API Ratings & Reviews", layout="wide")
+st.set_page_config(page_title="Explorateur API Ratings & Reviews : Analytics", layout="wide")
 
 st.session_state.setdefault("apply_filters", False)
 
